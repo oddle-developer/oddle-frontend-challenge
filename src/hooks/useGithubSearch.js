6bc3717ce5dd57517fc28aad7092ef4406dfe93d
范淼 <femiajayi705@gmail.com>
2021-03-14 23:39:48 +0800

    feat: Added the search hook

--- src/hooks/useGithubSearch.js
+++ src/hooks/useGithubSearch.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { fetchAUser } from "../redux/actions/searchActions";
+import useDebounce from "./useDebounce";
 import { useDispatch, useSelector } from "react-redux";
 const useGithubSearch = () => {
   const dispatch = useDispatch();
   const searchTerm = useSelector((state) => state.searchTerm);
+  const debouncedSearch = useDebounce(searchTerm, 400);
+
   useEffect(() => {
     dispatch(fetchAUser());
-  }, [dispatch, searchTerm]);
+  }, [dispatch, debouncedSearch]);
 };
 
 export default useGithubSearch;
