44310b9b22da99e791cc814cc41eb24d799be1bc
范淼 <femiajayi705@gmail.com>
2021-03-14 16:40:22 +0800

    refactor: Refacored the app component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import SearchBarContainer from "./Search/SearchBarContainer";
+
+const App = () => {
+  return (
+    <>
+      <SearchBarContainer />
+    </>
+  );
+};
+
+export default App;
