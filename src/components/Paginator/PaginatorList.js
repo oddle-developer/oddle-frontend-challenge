3ebede124e336cabc2ca7e73f571120b4e448ffd
范淼 <femiajayi705@gmail.com>
2021-03-15 12:54:06 +0800

    feat: Added initial paginator list

---
+++ src/components/Paginator/PaginatorList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import LoadingComponent from "../Loading/LoadingComponent";
+import useGetRepos from "../../hooks/useGetRepos";
+import Paginator from "./Paginator";
+const PaginatorList = ({ lists, loading, pagination }) => {
+  useGetRepos();
+  let { topicId } = useParams();
+
+  if (loading) {
+    return <LoadingComponent />;
+  }
+
+  const { currentPage, itemsPerPage } = pagination;
+
+  // Paginator Logic
+  const lastItemIndex = currentPage * itemsPerPage;
+  const firstItemIndex = lastItemIndex - itemsPerPage;
+  const currentListSlice = lists.slice(firstItemIndex, lastItemIndex);
+  return (
+    <>
+      <h3 className="text-primary mb-3">Viewing : {topicId} </h3>
+
+      <ul className="list-group mb-4">
+        {currentListSlice.map((item) => (
+          <li key={item.id} className="list-group-item">
+            {item.name}
+          </li>
+        ))}
+      </ul>
+      <Paginator />
+    </>
+  );
+};
+
+const mapStateToProps = ({ repos, loading, pagination }) => ({
+  loading,
+  lists: null || repos,
+  pagination,
+});
+
+export default connect(mapStateToProps)(PaginatorList);
