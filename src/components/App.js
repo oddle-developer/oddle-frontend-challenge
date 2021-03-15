5d9ec4c0e00728299f782cb08bf1a34957a299db
范淼 <femiajayi705@gmail.com>
2021-03-16 03:53:04 +0800

    refactor: Modified app to use css transition group

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,28 @@ import React from "react";
 import SearchBarContainer from "./Search/SearchBarContainer";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SearchResults from "./Search/SearchResults";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./App.scss";
 const App = () => {
   return (
-    <>
+    <div className="container">
       <BrowserRouter>
-        <Switch>
-          <Route path="/user/:username" component={SearchResults} />
-          <Route path="/">
-            <SearchBarContainer />
-          </Route>
-        </Switch>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={450} classNames="fade">
+                <Switch location={location}>
+                  <Route path="/user/:username" component={SearchResults} />
+                  <Route path="/">
+                    <SearchBarContainer />
+                  </Route>
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </BrowserRouter>
-    </>
+    </div>
   );
 };
 
