e3f132a4ea48401788dca0d46669b44e74f650a8
范淼 <femiajayi705@gmail.com>
2021-03-16 04:05:27 +0800

    refactor: Removed mock server response from followers actions

--- src/redux/actions/followersActions.js
+++ src/redux/actions/followersActions.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 import * as types from "../constants/actionTypes";
 import { githubUserUrl } from "../../config/appConfig";
-import { mockFollowersResponse } from "../../utils/utilFns";
+
 const fetchFollowers = () => {
   return async (dispatch, getState) => {
-    const { searchTerm } = getState();
+    const { user } = getState();
     dispatch({ type: types.LOADING_PENDING });
     try {
-      //const res = await axios.get(githubUserUrl + `${searchTerm}`);
-      //const res = await axios.get("http://jsonplaceholder.typicode.com/photos");
-      const res = await mockFollowersResponse();
-      console.log("The Followers result is :", res);
+      const res = await axios.get(
+        githubUserUrl + `${user.login}` + "/followers"
+      );
+
       const { data: followers } = res;
       dispatch({ type: types.LOADING_SUCCESS });
       dispatch({ type: types.SET_USER_FOLLOWERS, followers });
