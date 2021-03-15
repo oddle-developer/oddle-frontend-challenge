66865a36321f0a1f73457cb4bf52766decacf220
范淼 <femiajayi705@gmail.com>
2021-03-15 10:30:58 +0800

    feat: Added new reducers to the combined reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,15 @@ import errorReducer from "./errorReducer";
 import loadingReducer from "./loadingReducer";
 import searchReducer from "./searchReducer";
 import userReducer from "./userReducer";
-
+import repoReducer from "./reposReducer";
+import paginationReducer from "./paginationReducer";
 const rootReducer = combineReducers({
   searchTerm: searchReducer,
   error: errorReducer,
   loading: loadingReducer,
   user: userReducer,
+  repos: repoReducer,
+  pagination: paginationReducer,
 });
 
 export default rootReducer;
