c72dc7a27b937f9b62717504ac325347adfa3664
范淼 <femiajayi705@gmail.com>
2021-03-15 22:03:01 +0800

    feat: Added the repos compoenet

---
+++ src/components/User/Repos.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import PaginatorList from "../Paginator/PaginatorList";
+import useGetRepos from "../../hooks/useGetRepos";
+import { connect } from "react-redux";
+import LoadingComponent from "../Loading/LoadingComponent";
+const Repos = ({ repos, loading }) => {
+  let { username } = useParams();
+  useGetRepos();
+  return (
+    <>
+      <h3 className="text-primary mb-3">Viewing {username}&apos;s repos :</h3>
+      {loading && <LoadingComponent />}
+      {repos.length > 0 && <PaginatorList use="repos" lists={repos} />};
+    </>
+  );
+};
+
+const mapStateToProps = ({ repos, loading }) => ({
+  loading,
+  repos,
+});
+
+export default connect(mapStateToProps)(Repos);
