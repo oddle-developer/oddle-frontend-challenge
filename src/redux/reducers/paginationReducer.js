9b6ab990a074f6b5e7086620ba6f942642075240
范淼 <femiajayi705@gmail.com>
2021-03-15 12:57:36 +0800

    refactor: Modified paginator reducer fn

--- src/redux/reducers/paginationReducer.js
+++ src/redux/reducers/paginationReducer.js
@@ -1,21 +1,14 @@
-import {
-  SET_ITEMS_PER_PAGE,
-  SET_CURRENT_PAGE,
-  CLEAR_PAGINATION,
-} from "../constants/actionTypes";
+import { SET_CURRENT_PAGE, CLEAR_PAGINATION } from "../constants/actionTypes";
 const INITIAL_STATE = { itemsPerPage: 10, currentPage: 1 };
 
-const reposOnAdd = (repos) => {
-  console.log("The added user is :");
-  let updatedRecommendationItems = repos;
-  return updatedRecommendationItems;
+const currentPageSet = (state, number) => {
+  const newState = { ...state, currentPage: number };
+  return newState;
 };
 const paginationReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case SET_ITEMS_PER_PAGE:
-      return reposOnAdd(action.repos);
     case SET_CURRENT_PAGE:
-      return reposOnAdd(action.repos);
+      return currentPageSet(state, action.number);
     case CLEAR_PAGINATION:
       return INITIAL_STATE;
     default:
