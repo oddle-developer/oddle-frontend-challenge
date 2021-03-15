36fc8bfa6dc447900496ef15c8e807496e99b47b
范淼 <femiajayi705@gmail.com>
2021-03-15 20:33:55 +0800

    refactor: Modified the search results route

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -12,24 +12,18 @@ const SearchResults = ({ user }) => {
       <img src={user.avatar_url} className="search-avatar" alt="..." />
       <div>{user.login}</div>
       <div>
-        <Link to={`${match.url}searched/repos`}>
-          Public repos :{user.public_repos}
-        </Link>
+        <Link to={`${match.url}/repos`}>Public repos :{user.public_repos}</Link>
       </div>
       <div>
-        <Link to={`${match.url}searched/followers`}>
-          Followers :{user.followers}
-        </Link>
+        <Link to={`${match.url}/followers`}>Followers :{user.followers}</Link>
       </div>
       <div>
-        <Link to={`${match.url}searched/following`}>
-          Following :{user.following}
-        </Link>
+        <Link to={`${match.url}/following`}>Following :{user.following}</Link>
       </div>
       <hr />
       {/**Mode display below */}
       <Switch>
-        <Route path={`${match.path}searched/:topicId`}>
+        <Route path={`${match.path}/:topicId`}>
           <PaginatorList />
         </Route>
       </Switch>
