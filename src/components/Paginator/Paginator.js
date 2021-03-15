e03dff33732303c82e78b1b9e961d71958a46911
范淼 <femiajayi705@gmail.com>
2021-03-15 12:54:58 +0800

    refactor: Added paginator fns

--- src/components/Paginator/Paginator.js
+++ src/components/Paginator/Paginator.js
@@ -2,32 +2,62 @@ import React from "react";
 import { connect } from "react-redux";
 import { setCurrentPage } from "../../redux/actions/paginationActions";
 
-const Paginator = ({ itemsPerPage, totalList, setCurrentPage }) => {
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalList / itemsPerPage); i++) {
-    pageNumbers.push(i);
-  }
+const Paginator = ({ setCurrentPage, pagination, list }) => {
+  const { currentPage, itemsPerPage } = pagination;
+  const maxListNumber = Math.ceil(list.length / itemsPerPage);
+  const startListNumber = 1;
 
-  const paginate = (number) =>
-    console.log("setting number : ", number) || setCurrentPage(number);
+  const prevFn = () => {
+    if (currentPage > startListNumber) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextFn = () => {
+    if (currentPage < maxListNumber) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
     <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number} class="page-item">
-            <button class="page-link" onClick={() => paginate(number)}>
-              {number}
-            </button>
-          </li>
-        ))}
+      <ul className="pagination">
+        <li key={"prev"} className="page-item">
+          <button
+            disabled={currentPage === startListNumber ? true : false}
+            className="page-link"
+            onClick={prevFn}
+          >
+            <i className="fa fa-angle-double-left" aria-hidden="true"></i>
+          </button>
+        </li>
+        <li key={"..."} className="page-item">
+          <button className="page-link" disabled>
+            ...{" "}
+          </button>
+        </li>
+
+        <li key={"next"} className="page-item">
+          <button
+            disabled={currentPage === maxListNumber ? true : false}
+            className="page-link"
+            onClick={nextFn}
+          >
+            <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+          </button>
+        </li>
       </ul>
     </nav>
   );
 };
 
+const mapStateToProps = ({ pagination, repos }) => ({
+  pagination,
+  list: repos,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentPage: (number) => dispatch(setCurrentPage(number)),
 });
 
-export default connect(null, mapDispatchToProps)(Paginator);
+export default connect(mapStateToProps, mapDispatchToProps)(Paginator);
