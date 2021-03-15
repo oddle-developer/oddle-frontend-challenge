6153810592f6a63a89be7dfab891eb83499ed0a0
范淼 <femiajayi705@gmail.com>
2021-03-15 17:45:45 +0800

    feat: Added search results route to the app compoenet

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import SearchBarContainer from "./Search/SearchBarContainer";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import SearchResults from "./Search/SearchResults";
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
+          <Route path="/user/:username">
+            <SearchResults />
+          </Route>
           <Route path="/">
             <SearchBarContainer />
           </Route>
