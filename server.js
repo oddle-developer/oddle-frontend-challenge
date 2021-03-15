c52945694f2de5a59d9456bf94bf0b14d80022f4
范淼 <femiajayi705@gmail.com>
2021-03-16 05:38:35 +0800

    feat: Added server file for heroku deployment

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+app.set("port", process.env.PORT || 8080);
+
+const server = app.listen(app.get("port"), () => {
+  console.log("{^_^}/ listening on port ", server.address().port);
+});
