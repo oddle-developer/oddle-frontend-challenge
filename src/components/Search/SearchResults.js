bba7d1d3d38470aca4aac06e8f8583149e2cc578
范淼 <femiajayi705@gmail.com>
2021-03-15 13:02:54 +0800

    feat: Added react router to search result component

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -1,24 +1,38 @@
 import React from "react";
+import PaginatorList from "../Paginator/PaginatorList";
 import { connect } from "react-redux";
+import "./search.scss";
+import { useRouteMatch, Switch, Route, Link } from "react-router-dom";
 
 const SearchResults = ({ user }) => {
+  let match = useRouteMatch();
+  console.log("The match :", match);
   return (
     <div className="container">
-      <div className="mx-auto" style={{ width: "200px" }}>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={user.avatar_url} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{user.login}</h5>
-
-            <button
-              onClick={() => console.log("take me to the next page")}
-              className="btn btn-primary"
-            >
-              Go somewhere
-            </button>
-          </div>
-        </div>
+      <img src={user.avatar_url} className="search-avatar" alt="..." />
+      <div>{user.login}</div>
+      <div>
+        <Link to={`${match.url}searched/repos`}>
+          Public repos :{user.public_repos}
+        </Link>
       </div>
+      <div>
+        <Link to={`${match.url}searched/followers`}>
+          Followers :{user.followers}
+        </Link>
+      </div>
+      <div>
+        <Link to={`${match.url}searched/following`}>
+          Following :{user.following}
+        </Link>
+      </div>
+      <hr />
+      {/**Mode display below */}
+      <Switch>
+        <Route path={`${match.path}searched/:topicId`}>
+          <PaginatorList />
+        </Route>
+      </Switch>
     </div>
   );
 };
