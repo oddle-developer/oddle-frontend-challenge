a11a4037c9361ecaa3b584371d06be75aba19cc7
范淼 <femiajayi705@gmail.com>
2021-03-16 04:02:37 +0800

    refactor: Removed mock server response from repo actions

--- src/redux/actions/reposActions.js
+++ src/redux/actions/reposActions.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 import * as types from "../constants/actionTypes";
 import { githubUserUrl } from "../../config/appConfig";
-import { mockReposResponse } from "../../utils/utilFns";
 
 const fetchRepos = () => {
   return async (dispatch, getState) => {
-    const { searchTerm } = getState();
+    const { user } = getState();
     dispatch({ type: types.LOADING_PENDING });
     try {
-      //const res = await axios.get(githubUserUrl + `${searchTerm}`);
-      // const res = await axios.get(
-      //   "http://jsonplaceholder.typicode.com/comments"
-      // );
-      const res = await mockReposResponse();
+      const res = await axios.get(githubUserUrl + `${user.login}` + "/repos");
+
+      //const res = await mockReposResponse();
       console.log("The search term to fetch is :", res);
       const { data: repos } = res;
       dispatch({ type: types.LOADING_SUCCESS });
