c093b07ac35407bf69068621f1aaf8978d807eea
范淼 <femiajayi705@gmail.com>
2021-03-15 12:55:38 +0800

    feat: Added file loader for fontawesome icons

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: ["file-loader"],
+      },
     ],
   },
   plugins: [
